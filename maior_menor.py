12b2b3136cb1743d707785bf739d405f1c9cc67c
Icaro Nascimento <97711946+devnasc@users.noreply.github.com>
2023-11-22 19:49:09 -0300

    Add files via upload

---
+++ maior_menor.py
@@ -0,0 +1,6 @@
+n1 = int(input('Digite o primeiro numero: '))
+n2 = int(input('Digite o segundo numero: '))
+n3 = int(input('Digite o terceiro numero: '))
+lista = [n1,n2,n3]
+print(f'O menor valor digitado foi: {min(lista)}')
+print(f'O maior valor digitado foi: {max(lista)}')
